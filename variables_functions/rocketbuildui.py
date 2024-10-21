83ba7101ad8fd5b0934b01d644aec2b8ddff28a3
BuildTools <unconfigured@null.spigotmc.org>
2024-10-21 16:52:01 +0100

    message

---
+++ variables_functions/rocketbuildui.py
@@ -0,0 +1,48 @@
+##--IMPORTS--##
+
+import json
+import pygame
+import pymunk
+import keyboard
+from variables_functions import variables
+from variables_functions import ui_elements
+
+##--BUILDING FUNCTIONS--##
+parts = []
+def make_commandpod():
+    parts.append(["commandpodussr", variables.screen_width/2, 0+variables.numofparts*128])
+    variables.numofparts += 1
+
+def make_fueltank():
+    parts.append(["fueltankru", variables.screen_width/2, 0+variables.numofparts*128])
+    variables.numofparts += 1
+        
+        
+##--CLASS BUILDUI--##
+
+#class BuildUI:
+#    def __init__(self, screen):
+#        self.screen = screen
+#        self.command = None#
+#
+#    def buildUI(self):
+#        self.command = ui_elements.Button(variables.screen, 20, 20, 100, 50,
+#                           variables.white, variables.black, "consolas", 15, 20, "Capsule",
+#                           variables.commandbuttonimg, "summon command module", make_commandpod,
+#                           hide_fill=True, outline=False, outlinecolor=(255, 255, 255), outlinethickness=5)
+
+def build_ui():
+    variables.buttons.append(ui_elements.Button(variables.screen, 20, 20, 100, 50,
+                           variables.white, variables.black, "consolas", 15, 20, "Make Command Module",
+                           variables.blank, "summon command module", make_commandpod,
+                           hide_fill=False, outline=False, outlinecolor=(255, 255, 255), outlinethickness=5))
+    variables.buttons.append(ui_elements.Button(variables.screen, 20, 140, 100, 50,
+                           variables.white, variables.black, "consolas", 15, 20, "Make Fuel Tank",
+                           variables.blank, "summon fuel tank", make_fueltank,
+                           hide_fill=False, outline=False, outlinecolor=(255, 255, 255), outlinethickness=5))
+
+def update():
+    for part in parts:
+        #variables.screen.blit(variables.images[part[0]], (variables.screen_width/2, variables.screen_height-10+variables.numofparts*31))
+        variables.screen.blit(variables.images[part[0]],
+                              (part[1], part[2]))
